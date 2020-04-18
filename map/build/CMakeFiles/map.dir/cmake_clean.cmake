ca74284e6f68be588d6f8842ed846a5de16b9fed
liorberko <63656016+liorberko@users.noreply.github.com>
2020-04-18 19:55:19 +0300

    Add initial version of map.c project

---
+++ map/build/CMakeFiles/map.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/map.dir/map.c.obj"
+  "libmap.a"
+  "libmap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/map.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
